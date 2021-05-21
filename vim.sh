4b9eb1bb5fd076144eee41210b1376aea8f0c3f8
BitFur <bitfur@users.noreply.github.comv>
2021-05-22 00:02:45 +0300

    added vim config

---
+++ vim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cat <<EOF > ~/.vimrc
+set ai
+set encoding=utf-8
+set ts=4
+set sw=4
+syn on
+"set t_Co=0
+"set t_md=
+set nowrap
+set shortmess=atI
+set expandtab
+set background=dark
+set scrolloff=999
+EOF
+
