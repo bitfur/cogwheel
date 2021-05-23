0827c512dc5d189a03069a54c5db0f4feec557d3
BitFur <bitfur@users.noreply.github.comv>
2021-05-23 13:34:36 +0300

    updates

---
+++ Arch-cache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Broken!
+#pacman -S squid
+cat <<EOF > /etc/squid/squid.conf
+refresh_pattern \.pkg\.tar\.   0       20%     4320      reload-into-ims
+refresh_pattern .              0       0%      0
+maximum_object_size 256 MB
+cache_dir aufs /var/cache/squid 10000 16 256
+shutdown_lifetime 1 seconds 
+http_port 3128 intercept
+http_port 3129
+EOF
+
+
